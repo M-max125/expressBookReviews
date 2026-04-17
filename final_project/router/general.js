2a0d3b9a95bdf6acf964e17d4822574e46e74e5f
M-max125 <c.marcu60@yahoo.com>
2026-04-17 15:46:08 -0400

    Optimized general.js file with promises and asyn await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,32 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getAllBooks = () => {
+    return new Promise((resolve) => {
+        resolve(books);
+    });
+}
+
+const getBookByISBN = (isbn) => {
+    return new Promise((resolve) => {
+        resolve(books[isbn]);
+    });
+}
+
+const getBookByAuthor = (author) => {
+    return new Promise((resolve) => {
+        const book = Object.values(books).find(b => b.author === author);
+        resolve(book);
+    });
+}
+
+const getBookByTitle = (title) => {
+    return new Promise((resolve) => {
+        const book = Object.values(books).find(b => b.title === title);
+        resolve(book);
+    });
+}
+
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -41,39 +67,44 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4));
+  const allBooks = await getAllBooks();
+  return res.json(allBooks);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  return res.send(books[isbn]);
+  const book = await getBookByISBN(isbn);
+  if (!book) {
+    return res.status(404).json({message: "Book not found"});
+  }
+  return res.json(book);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   //Write your code here
   const author = req.params.author;
-  const book = Object.values(books).find(b => b.author === author);
-  if (book) {
-    return res.send(book);
-  } else {
-    return res.status(404).json({message: " There is no book by this author "});  }
+  const book = await getBookByAuthor(author);
+  if (!book) {
+    return res.status(404).json({message: "No book with this author"});
+  }
+  return res.json(book);
   
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
   const title = req.params.title;
-  const book = Object.values(books).find(b => b.title === title);
-  if (book) {
-    return res.send(book);
-  } else {
-    return res.status(404).json({message: " There is no book with this title "});  }
+  const book = await getBookByTitle(title);
+  if (!book) {
+    return res.status(404).json({message: "No book with this title"});
+  }
+  return res.json(book);
 });
 
 //  Get book review
