4d487c0e1f5ced4d1f32b326b89b77fac27acf60
M-max125 <c.marcu60@yahoo.com>
2026-04-17 16:27:46 -0400

    Added try catch logic

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -69,42 +69,59 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
   //Write your code here
-  const allBooks = await getAllBooks();
-  return res.json(allBooks);
+  try {
+    const allBooks = await getAllBooks();
+    return res.json(allBooks);
+  } catch (error) {
+    return res.status(500).json({message: "Internal server error"});
+  }
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-  const book = await getBookByISBN(isbn);
-  if (!book) {
-    return res.status(404).json({message: "Book not found"});
+  try {
+    const isbn = req.params.isbn;
+    const book = await getBookByISBN(isbn);
+    if (!book) {
+      return res.status(404).json({message: "Book not found"});
+    }
+    return res.json(book);
+  } catch(error) {
+    return res.status(500).json({message: "Internal server error"});
   }
-  return res.json(book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
   //Write your code here
-  const author = req.params.author;
-  const book = await getBookByAuthor(author);
-  if (!book) {
-    return res.status(404).json({message: "No book with this author"});
+  try {
+    const author = req.params.author;
+    const book = await getBookByAuthor(author);
+    if (!book) {
+      return res.status(404).json({message: "No book with this author"});
+    }
+    return res.json(book);
+  } catch (error) {
+    return res.status(500).json({message: "Internal server error"});
   }
-  return res.json(book);
   
 });
 
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  const book = await getBookByTitle(title);
-  if (!book) {
-    return res.status(404).json({message: "No book with this title"});
+  try {
+    const title = req.params.title;
+    const book = await getBookByTitle(title);
+    if (!book) {
+      return res.status(404).json({message: "No book with this title"});
+    }
+    return res.json(book);
+  } catch (error) {
+    return res.status(500).json({message: "Internal server error"});
   }
-  return res.json(book);
 });
 
 //  Get book review
