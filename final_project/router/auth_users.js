b4485f2a29569cea3e3668f09149856f54ba13b3
M-max125 <c.marcu60@yahoo.com>
2026-04-17 14:51:48 -0400

    More modifications

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,62 @@ const regd_users = express.Router();
 
 let users = [];
 
+const JWT_SECRET = "user_secret_key";
+
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+const regex = /^[a-zA-Z0-9]{3,}$/;
+return regex.test(username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+return users.find(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  console.log(users);
+  const { username, password} = req.body;
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password required"});
+  }
+  const user = authenticatedUser(username, password);
+  if (!user) {
+    return res.status(401).json({message: "Invalid credentials"});
+  }
+
+  // create JWT token
+  const token  = jwt.sign({username: user.username}, JWT_SECRET, {expiresIn: "1h"});
+  return res.json({
+    message: "Login successfull",
+    token
+  })
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.user.username;
+  // validate review
+  if (!review) {
+    return res.status(400).json({message: "Review is required in query"});
+  }
+  // check if book exists
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({message: "Book not found"});
+  }
+  // add or update review
+  book.reviews[username] = review;
+  return res.json({
+    message: "Review added/updated successfully",
+    isbn,
+    reviews: book.reviews
+  })
 });
 
 module.exports.authenticated = regd_users;
